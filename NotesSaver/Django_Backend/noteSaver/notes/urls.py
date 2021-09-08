707e7725d111efdeb2566d44fe51623ccf630bb3
Jayesh <jayeshrathi6055@gmail.com>
2021-09-08 09:35:46 +0530

    Make API (create and read operation)

--- NotesSaver/Django_Backend/noteSaver/notes/urls.py
+++ NotesSaver/Django_Backend/noteSaver/notes/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('save/', views.save),
+    path('showAll/', views.show),
 ]
--- NotesSaver/Django_Backend/noteSaver/notes/views.py
+++ NotesSaver/Django_Backend/noteSaver/notes/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from .models import Notes
+import json
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Hello world") 
+
+# Create
+def save(request):
+    body = request.body.decode('utf-8')
+    body = json.loads(body)
+    new = Notes(title = body['title'], description = body['description'])
+    new.save()
+    obj = {"title":new.title,"description":new.description}
+    return JsonResponse(obj)
+
+# Read
+def show(request):
+    arr_of_obj = []
+    arr = Notes.objects.all()
+    for i in arr:
+        o = {"title":i.title,"description":i.description}
+        arr_of_obj.append(o)
+
+    return JsonResponse(arr_of_obj,safe=False)
+
+# Update
+
+# Delete
